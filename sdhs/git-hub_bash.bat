9d9025b410eb8ccf955f7ac61a6fc9d7887a25ca
17bui15 <17ubi15@sdh.hs.kr>
2018-04-16 10:24:12 +0900

    2018-04-16

---
+++ sdhs/git-hub_bash.bat
@@ -0,0 +1,7 @@
+@echo off
+
+start git config --global user.email "17ubi15@sdh.hs.kr"
+start git config --global user.name "17bui15"
+
+echo Done!
+pause
--- 박형진 학교홈페이지/git-hub_bash.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-start git config --global user.email "17ubi15@sdh.hs.kr"
-start git config --global user.name "17bui15"
-
-echo Done!
-pause
