00e943244f22d9df58235620b8fd1b55c06d5aab
17bui15 <17ubi15@sdh.hs.kr>
2018-04-23 15:39:32 +0900

    2018-04-23

--- sdhs/js/index.js
+++ sdhs/js/index.js
@@ -1,3 +1,4 @@
+// 이미지 슬라이드
 var img = $("#slide_box-content img");
 var cnt = 0;
 var max = img.length-1;
@@ -13,10 +14,30 @@ setInterval(function(){
     $(img[cnt]).animate({"left":"0"});
 },3000);
 
+// 날짜
 var today = new Date();
 var year = today.getFullYear();
-var month = today.getMonth();
+var month = today.getMonth()+1;
 var day = today.getDate();
 
-$("#calendar_year").append(year+'년');
-$("#calendar_month").append(month+'월');
+$(".year").append(year+'년');
+$(".month").append(month+'월');
+$(".day").append(day+'일');
+var thisweek = today.getDay();
+var date = ["월","화","수","목","금","토","일"];
+$(".week").append(date[thisweek-1]);
+
+// 지도
+function map(){
+    function initMap() {
+        var uluru = {lat: 37.539088, lng: 126.990692};
+        var map = new google.maps.Map(document.getElementById('map_box'), {
+          zoom: 4,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
+}
