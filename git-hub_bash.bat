b710d8f2eb23cded4407406e038f5ace71b5da2d
ParkhJ <37681882+hJ6129@users.noreply.github.com>
2018-06-14 10:03:33 +0900

    email, username change

--- git-hub_bash.bat
+++ git-hub_bash.bat
@@ -1,7 +1,7 @@
 @echo off
 
-start git config --global user.email "17ubi15@sdh.hs.kr"
-start git config --global user.name "17bui15"
+start git config --global user.email "zxqkrgudwls@naver.com"
+start git config --global user.name "hJ6129"
 
 echo Done!
 pause
